3b746fe9bc7afa972cde1b62bb0e4d11505f3d19
Andrew Abogado <me@andrewabogado.com>
2010-11-18 23:40:28 +0800

    Removed /public/javascripts/tip.js. Instead updated by tips.js on root with my changes.

--- tips.js
+++ tips.js
@@ -537,4 +537,18 @@ tips.push({
     ]
 });
 
+tips.push({
+    author: 'andrewabogado',
+    message: 'Wrap around a div element in an anchor element to ensure <em>kick-ass block-level link</em>',
+    example: [
+        '<div>',
+        '   <div id="left_menu">',
+        '       <a href="#">',
+        '         <div> Hi, Im a block level link</div>',
+        '       </a>',
+        '   </div>',
+        '</div>'
+    ]
+});
+
 module.exports = tips;
