8589ef38daffe2ec5aff64bc0d178cfd02e30dca
Andrew Abogado <me@andrewabogado.com>
2010-11-18 23:33:37 +0800

    Added with this message "Wrap around a div element in an anchor element to ensure kick-ass block-level link". Contribute by Andrew Abogado.

---
+++ public/javascripts/tips.js
@@ -0,0 +1,13 @@
+tips.push({
+    author: 'andrewabogado',
+    message: 'Wrap around a div element in an anchor element to ensure kick-ass block-level link',
+    example: [
+        '<div>',
+          '<div id="left_menu">',
+            '<a href="#">',
+              '<div> Hi, Im a block level link</div>',
+            '</a>',
+          '</div>',
+        '</div>'
+    ]
+});
